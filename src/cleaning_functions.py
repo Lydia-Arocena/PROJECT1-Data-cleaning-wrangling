6378095813c5aff82d1efbafd62cd3455eee6fe1
Lydia-Arocena <lydiaarocena@gmail.com>
2021-10-30 20:46:15 +0200

    Sábado:terminada columna Sexo, limpieza columna Especies, función hemisferios

--- src/cleaning_functions.py
+++ src/cleaning_functions.py
@@ -1,7 +1,22 @@
-AQUÍ SOLO VOY A METER LAS FUNCIONES QUE VAYA A UTILIZAR (IMPORTO LIBRERÍAS QUE VAYA A USAR TAMBIÉN).
-
 import pandas as pd
 import numpy as np
 import seaborn as sb
-df = pd.read_csv("DATA/attacks.csv",encoding = "ISO-8859-1")
 
+
+def hemisferio (x):
+        """
+        Esta función devuelve el hemisferio donde se encuentra un país.
+
+        Args:
+        x(string):país.
+    
+        Return: El hemisferio.
+    
+        """
+        
+        south = ["Angola", "Argentina", "Australia", "Bolivia", "Botswana", "Brazil", "Burundi", "Chile", "Colombia", "Comoros", "Ecuador", "Fiji" , "Gabon", "Equatorial Guinea", "Indonesia", "Solomon Islands", "Kenya", "Kiribati", "Lesotho", "Madagascar", "Malawi", "Maldives", "Mauritius", "Mozambique" , "Namibia", "Nauru", "New Zealand", "Papua New Guinea", "Paraguay", "Peru", "Republic of the Congo", "Democratic Republic of the Congo", "Rwanda", "Samoa", " Sao Tome and Principe "," Seychelles "," Somalia "," Swaziland "," South Africa "," Tanzania "," East Timor "," Tonga "," Tuvalu "," UgandaUruguay "," Vanuatu "," Zambia " ,"Zimbabwe"]
+
+        if x.issuper() in south:
+            return "Hemisferio sur"
+        else:
+            return "Hemisferio norte"
